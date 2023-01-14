c043c3ca2b565b6835a19b5e6155eedad0f54e93
JXG052 <jxg0521@gmail.com>
2023-01-14 07:38:53 +0000

    added animation

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const modal = document.querySelector("#container");
 const btn = document.querySelector(".btn");
 const closeBtn = document.querySelector("#close-btn");
 
+
+//displays modal when button is clicked
 btn.addEventListener("click", function (){
   modal.style.display = "block"
 })
 
+// hides modal when x button is clicked
 closeBtn.addEventListener("click", function () {
     modal.style.display = "none"
 })
 
+// hides modal when anywhere on screen is clicked
 window.addEventListener('click', function(e) {
     if ( e.target === modal){
         modal.style.display = "none";
